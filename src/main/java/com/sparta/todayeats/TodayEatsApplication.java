c2c2061bfe44a03177446c329edfc825e6f375ce
GolemOnce <shh3350@gmail.com>
2026-04-17 11:34:48 +0900

    초기 환경 설정

---
+++ src/main/java/com/sparta/todayeats/TodayEatsApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.todayeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayEatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodayEatsApplication.class, args);
+    }
+
+}
