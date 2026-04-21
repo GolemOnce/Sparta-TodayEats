2ec1316dc5dee1955f607a515abda2e2b436f259
gray-ji <seeu314@gmail.com>
2026-04-21 22:59:00 +0900

    refactor: GlobalExceptionHandler 응답 구조 개선

--- src/main/java/com/sparta/todayeats/global/infrastructure/presentation/advice/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/todayeats/global/infrastructure/presentation/advice/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.sparta.todayeats.global.infrastructure.presentation.advice;
 
 import com.sparta.todayeats.global.exception.BaseException;
+import com.sparta.todayeats.global.exception.CommonErrorCode;
 import com.sparta.todayeats.global.exception.ErrorCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,25 +14,21 @@ import java.util.Map;
 public class GlobalExceptionHandler {
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<?> handleBaseException(BaseException e) {
-        ErrorCode errorCode = e.getErrorCode();
+        return buildResponse(e.getErrorCode());
+    }
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleException(Exception e) {
+        return buildResponse(CommonErrorCode.INTERNAL_SERVER_ERROR);
+    }
+
+    private ResponseEntity<?> buildResponse(ErrorCode errorCode) {
         return ResponseEntity
                 .status(errorCode.getStatus())
                 .body(Map.of(
                         "timestamp", LocalDateTime.now(),
                         "code", errorCode.getCode(),
                         "message", errorCode.getMessage()
-                ));
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> handleException(Exception e) {
-        return ResponseEntity
-                .status(500)
-                .body(Map.of(
-                        "timestamp", LocalDateTime.now(),
-                        "code", "INTERNAL_SERVER_ERROR",
-                        "message", "서버 내부 오류가 발생했습니다."
                 ));
     }
 }
