5f35aae338ad572f71fb884c55f1e7567da8734e
dddd2356 <dddd2356@naver.com>
2026-04-22 13:08:38 +0900

    feat: 주문 도메인 엔티티 추가

--- src/main/java/com/sparta/todayeats/global/exception/OrderErrorCode.java
+++ src/main/java/com/sparta/todayeats/global/exception/OrderErrorCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum OrderErrorCode implements ErrorCode {
     ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "ORDER-001", "주문을 찾을 수 없습니다."),
-    CANCEL_TIME_EXCEEDED(HttpStatus.BAD_REQUEST, "ORDER-002", "취소 가능 시간이 지났습니다.");
+    CANCEL_TIME_EXCEEDED(HttpStatus.BAD_REQUEST, "ORDER-002", "취소 가능 시간이 지났습니다."),
+    INVALID_ORDER_STATUS(HttpStatus.BAD_REQUEST, "ORDER-003", "유효하지 않은 주문 상태 변경입니다."),
+    ORDER_CANCEL_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "ORDER-004", "주문 수락 이후에는 취소할 수 없습니다.");
 
     private final HttpStatus status;
     private final String code;
