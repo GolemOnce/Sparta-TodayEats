1e9fbc56f3f94b65ef6174d96879bf31d626819b
yuna <micha0215@tukorea.ac.kr>
2026-04-22 14:03:34 +0900

    feat: 공통 응답(ApiResponse) 구조 추가

---
+++ src/main/java/com/sparta/todayeats/global/response/ApiResponse.java
@@ -0,0 +1,25 @@
+package com.sparta.todayeats.global.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ApiResponse<T> {
+
+    private int status;
+    private String message;
+    private T data;
+
+    public static <T> ApiResponse<T> created(T data) {
+        return new ApiResponse<>(201, "CREATED", data);
+    }
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(200, "SUCCESS", data);
+    }
+
+    public static <T> ApiResponse<T> deleted(T data) {
+        return new ApiResponse<>(204, "NO_CONTENT", null);
+    }
+}
