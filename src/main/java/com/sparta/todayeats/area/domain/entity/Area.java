b34c4d97f54a38629fd058c5b4130d842a4bcf16
yuna <micha0215@tukorea.ac.kr>
2026-04-23 16:28:35 +0900

    feat: area 엔티티 추가

---
+++ src/main/java/com/sparta/todayeats/area/domain/entity/Area.java
@@ -0,0 +1,43 @@
+package com.sparta.todayeats.area.domain.entity;
+
+import com.sparta.todayeats.global.infrastructure.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Where;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "p_area")
+@Getter
+@Builder
+@AllArgsConstructor
+@Where(clause = "deleted_at is null")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Area extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "area_id")
+    private UUID id;
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String name;
+
+    @Column(nullable = false, length = 50)
+    private String city;
+
+    @Column(nullable = false, length = 50)
+    private String district;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Boolean isActive = true;
+
+    public void update(String name, String city, String district, Boolean isActive) {
+        this.name = name;
+        this.city = city;
+        this.district = district;
+        this.isActive = isActive;
+    }
+}
