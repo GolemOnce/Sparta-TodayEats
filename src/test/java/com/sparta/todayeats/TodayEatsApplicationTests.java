be91783218cde7a02ae7b40245162399b36b0842
yuna <micha0215@tukorea.ac.kr>
2026-04-23 14:36:08 +0900

    test: 테스트코드 오류 수정
    테스트용 DB 필요

--- src/test/java/com/sparta/todayeats/TodayEatsApplicationTests.java
+++ src/test/java/com/sparta/todayeats/TodayEatsApplicationTests.java
@@ -2,7 +2,9 @@ package com.sparta.todayeats;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class TodayEatsApplicationTests {
 
