9c8f0b4f655e4bd436391ae77c1eb7426b3c1ab1
gray-ji <seeu314@gmail.com>
2026-04-24 13:32:29 +0900

    refactor: SignupResponse 구조 개선

--- src/main/java/com/sparta/todayeats/auth/presentation/dto/response/SignupResponse.java
+++ src/main/java/com/sparta/todayeats/auth/presentation/dto/response/SignupResponse.java
@@ -5,7 +5,6 @@ import com.sparta.todayeats.user.domain.entity.UserRoleEnum;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Getter
 public class SignupResponse {
@@ -13,13 +12,11 @@ public class SignupResponse {
     private final String nickname;
     private final UserRoleEnum role;
     private final LocalDateTime createdAt;
-    private final UUID createdBy;
 
     public SignupResponse(User user) {
         this.email = user.getEmail();
         this.nickname = user.getNickname();
         this.role = user.getRole();
         this.createdAt = user.getCreatedAt();
-        this.createdBy = user.getCreatedBy();
     }
 }
