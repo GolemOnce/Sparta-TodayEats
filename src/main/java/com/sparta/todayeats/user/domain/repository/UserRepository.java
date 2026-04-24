e8eb46abffcfea000c0a11e17bfd109c1450a2a0
gray-ji <seeu314@gmail.com>
2026-04-24 13:31:28 +0900

    fix: 회원가입 role 검증 및 repository 타입 오류 수정

--- src/main/java/com/sparta/todayeats/user/domain/repository/UserRepository.java
+++ src/main/java/com/sparta/todayeats/user/domain/repository/UserRepository.java
@@ -8,5 +8,5 @@ import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     boolean existsByEmail(String email);
-    Optional<Object> findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
