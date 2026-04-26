0989bb52d247859a10a32168a641d50fbe7cc97c
gray-ji <seeu314@gmail.com>
2026-04-26 16:12:08 +0900

    refactor: Request DTO 역직렬화 에러 대응

--- src/main/java/com/sparta/todayeats/auth/presentation/dto/response/SignupResponse.java
+++ src/main/java/com/sparta/todayeats/auth/presentation/dto/response/SignupResponse.java
@@ -10,13 +10,13 @@ import java.time.LocalDateTime;
 public class SignupResponse {
     private final String email;
     private final String nickname;
-    private final UserRoleEnum role;
+    private final String role;
     private final LocalDateTime createdAt;
 
     public SignupResponse(User user) {
         this.email = user.getEmail();
         this.nickname = user.getNickname();
-        this.role = user.getRole();
+        this.role = user.getRole().name();
         this.createdAt = user.getCreatedAt();
     }
 }
