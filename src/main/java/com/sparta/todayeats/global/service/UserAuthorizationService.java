49210416dc818896b1cc780c37574844b06b71f5
gray-ji <seeu314@gmail.com>
2026-04-26 10:33:48 +0900

    feat: UserAuthorizationService 구현

---
+++ src/main/java/com/sparta/todayeats/global/service/UserAuthorizationService.java
@@ -0,0 +1,61 @@
+package com.sparta.todayeats.global.service;
+
+import com.sparta.todayeats.global.exception.AuthErrorCode;
+import com.sparta.todayeats.global.exception.BaseException;
+import com.sparta.todayeats.global.exception.UserErrorCode;
+import com.sparta.todayeats.user.domain.entity.User;
+import com.sparta.todayeats.user.domain.entity.UserRoleEnum;
+import com.sparta.todayeats.user.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class UserAuthorizationService {
+    private final UserRepository userRepository;
+
+    public User getUserById(UUID userId) {
+        return userRepository.findByUserId(userId)
+                .orElseThrow(() -> new BaseException(UserErrorCode.USER_NOT_FOUND));
+    }
+
+    // 권한 검증
+    public void validateMaster(User user) {
+        if (!isMaster(user)) {
+            throw new BaseException(AuthErrorCode.FORBIDDEN);
+        }
+    }
+
+    public void validateAdmin(User user) {
+        if (!isAdmin(user)) {
+            throw new BaseException(AuthErrorCode.FORBIDDEN);
+        }
+    }
+
+    public void validateSelf(UUID targetUserId, UUID currentUserId) {
+        if (!targetUserId.equals(currentUserId)) {
+            throw new BaseException(AuthErrorCode.FORBIDDEN);
+        }
+    }
+
+    // 권한 확인
+    public boolean isOwner(User user) {
+        return user.getRole() == UserRoleEnum.OWNER;
+    }
+
+    public boolean isMaster(User user) {
+        return user.getRole() == UserRoleEnum.MASTER;
+    }
+
+    public boolean isManager(User user) {
+        return user.getRole() == UserRoleEnum.MANAGER;
+    }
+
+    public boolean isAdmin(User user) {
+        return isMaster(user) || isManager(user);
+    }
+}
