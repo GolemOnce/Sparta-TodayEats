dea01e249eec17415a0ba3be8154e322ad0cb241
GolemOnce <shh3350@gmail.com>
2026-04-26 23:54:08 +0900

    feat: 결제 처리 API 구현

---
+++ src/main/java/com/sparta/todayeats/payment/repository/PaymentRepository.java
@@ -0,0 +1,17 @@
+package com.sparta.todayeats.payment.repository;
+
+import com.sparta.todayeats.order.entity.Order;
+import com.sparta.todayeats.payment.entity.Payment;
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, UUID> {
+}
