a0628fd3a8fbe05bcefc271d0fde302d25a04370
gray-ji <seeu314@gmail.com>
2026-04-27 12:06:38 +0900

    feat: 비밀번호 변경 로직 구현

--- src/main/java/com/sparta/todayeats/user/service/UserService.java
+++ src/main/java/com/sparta/todayeats/user/service/UserService.java
@@ -1,16 +1,21 @@
 package com.sparta.todayeats.user.service;
 
+import com.sparta.todayeats.global.exception.BaseException;
+import com.sparta.todayeats.global.exception.UserErrorCode;
 import com.sparta.todayeats.global.service.UserAuthorizationService;
 import com.sparta.todayeats.global.util.PageableUtils;
 import com.sparta.todayeats.user.domain.entity.User;
 import com.sparta.todayeats.user.domain.entity.UserRoleEnum;
 import com.sparta.todayeats.user.domain.repository.UserRepository;
+import com.sparta.todayeats.user.dto.request.UpdatePasswordRequest;
 import com.sparta.todayeats.user.dto.request.UpdateUserRequest;
+import com.sparta.todayeats.user.dto.response.UpdatePasswordResponse;
 import com.sparta.todayeats.user.dto.response.UpdateUserResponse;
 import com.sparta.todayeats.user.dto.response.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +27,7 @@ import java.util.UUID;
 public class UserService {
     private final UserAuthorizationService userAuthorizationService;
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public Page<UserResponse> searchUsers(
             String keyword, UserRoleEnum role, Pageable pageable, UUID currentUserId
@@ -62,5 +68,33 @@ public class UserService {
         user.update(request.getNickname(), request.isVisible());
 
         return new UpdateUserResponse(user);
+    }
+
+    @Transactional
+    public UpdatePasswordResponse updatePassword(UpdatePasswordRequest request, UUID userId) {
+        // 사용자 조회
+        User user = userAuthorizationService.getUserById(userId);
+
+        // 현재 비밀번호 일치 확인
+        String currentPassword = request.getCurrentPassword();
+        if (!passwordEncoder.matches(currentPassword, user.getPassword())) {
+            throw new BaseException(UserErrorCode.PASSWORD_MISMATCH);
+        }
+
+        // 새 비밀번호 일치 확인
+        String newPassword = request.getNewPassword();
+        if (!newPassword.equals(request.getConfirmNewPassword())) {
+            throw new BaseException(UserErrorCode.PASSWORD_MISMATCH);
+        }
+
+        // 현재 비밀번호와 새 비밀번호 일치 확인
+        if (currentPassword.equals(newPassword)) {
+            throw new BaseException(UserErrorCode.DUPLICATE_PASSWORD);
+        }
+
+        // 비밀번호 변경
+        user.updatePassword(passwordEncoder.encode(newPassword));
+
+        return new UpdatePasswordResponse(user);
     }
 }
