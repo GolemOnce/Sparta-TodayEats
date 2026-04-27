a9c6479c8585a2deafda8d45d6228c0b4df15d84
yuna <micha0215@tukorea.ac.kr>
2026-04-27 17:47:15 +0900

    feat: 가게 권한 처리

--- src/main/java/com/sparta/todayeats/store/controller/StoreController.java
+++ src/main/java/com/sparta/todayeats/store/controller/StoreController.java
@@ -16,6 +16,8 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -27,11 +29,10 @@ public class StoreController {
 
     private final StoreService storeService;
 
-    // TODO: 권한 처리(OWNER), Auditing
-    // TODO: 인증 구현 후 토큰에서 꺼내기 -> userId
     // 가게 생성
     @PostMapping
-    public ResponseEntity<ApiResponse<StoreCreateResponse>> createStore(@Valid @RequestBody StoreCreateRequest request, @RequestHeader("X-User-Id") UUID userId) {
+    @PreAuthorize("hasAnyRole('OWNER')")
+    public ResponseEntity<ApiResponse<StoreCreateResponse>> createStore(@Valid @RequestBody StoreCreateRequest request,@AuthenticationPrincipal UUID userId) {
         StoreCreateResponse response = storeService.createStore(request, userId);
 
         return ResponseEntity
@@ -39,7 +40,6 @@ public class StoreController {
                 .body(ApiResponse.created(response));
     }
 
-    // TODO: Auditing
     // TODO: CUSTOMER → isHidden = false 인 것만 노출
     // TODO: OWNER/MANAGER/MASTER → 숨김 포함 전체 노출
     // 가게 목록 조회 + 복합 검색 (카테고리, 이름)
@@ -52,8 +52,7 @@ public class StoreController {
         PageResponse<StoreResponse> response = storeService.getStores(categoryName, keyword, pageable);
         return ResponseEntity.ok(ApiResponse.success(response));
     }
-
-    // TODO: Auditing
+    
     // 가게 단건 조회
     @GetMapping("/{storeId}")
     public ResponseEntity<ApiResponse<StoreResponse>> getStore(
@@ -64,42 +63,35 @@ public class StoreController {
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
-    // TODO: 권한 처리(OWNER, MANAGER, MASTER), Auditing
-    // TODO: 인증 구현 후 토큰에서 꺼내기 -> userId
     // 가게 수정
     @PatchMapping("/{storeId}")
+    @PreAuthorize("hasAnyRole('OWNER','MANAGER','MASTER')")
     public ResponseEntity<ApiResponse<StoreResponse>> updateStore(
             @PathVariable UUID storeId,
-            @Valid @RequestBody StoreUpdateRequest request,
-            @RequestHeader("X-User-Id") UUID userId) {
+            @Valid @RequestBody StoreUpdateRequest request,@AuthenticationPrincipal UUID userId) {
 
         StoreResponse response = storeService.updateStore(storeId, request,userId);
 
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
-    // TODO: 권한 처리(OWNER, MASTER), Auditing
-    // TODO: 인증 구현 후 토큰에서 꺼내기 -> userId
     // 가게 삭제
     @DeleteMapping("/{storeId}")
+    @PreAuthorize("hasAnyRole('OWNER','MASTER')")
     public ResponseEntity<ApiResponse<Void>> deleteStore(
-            @PathVariable UUID storeId,
-            @RequestHeader("X-User-Id") UUID userId) {
+            @PathVariable UUID storeId,@AuthenticationPrincipal UUID userId) {
 
         storeService.deleteStore(storeId, userId);
 
         return ResponseEntity.noContent().build();
     }
 
-
-    // TODO: 권한 처리(OWNER, MANAGER, MASTER), Auditing
-    // TODO: 인증 구현 후 토큰에서 꺼내기 -> userId
     // 가게 숨김 처리
     @PatchMapping("/{storeId}/hide")
+    @PreAuthorize("hasAnyRole('OWNER','MANAGER','MASTER')")
     public ResponseEntity<ApiResponse<StoreHiddenResponse>> updateHidden(
             @PathVariable UUID storeId,
-            @Valid @RequestBody StoreHiddenRequest request,
-            @RequestHeader("X-User-Id") UUID userId) {
+            @Valid @RequestBody StoreHiddenRequest request,@AuthenticationPrincipal UUID userId) {
 
         StoreHiddenResponse response = storeService.updateHidden(storeId, request, userId);
 
