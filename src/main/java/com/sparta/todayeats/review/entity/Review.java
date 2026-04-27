65aba3dd588743143c09311b69825c121b60853f
GolemOnce <shh3350@gmail.com>
2026-04-27 23:14:36 +0900

    feat: 리뷰 엔티티 생성

---
+++ src/main/java/com/sparta/todayeats/review/entity/Review.java
@@ -0,0 +1,40 @@
+package com.sparta.todayeats.review.entity;
+
+import com.sparta.todayeats.global.infrastructure.entity.BaseEntity;
+import com.sparta.todayeats.order.entity.Order;
+import com.sparta.todayeats.store.entity.Store;
+import com.sparta.todayeats.user.domain.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "p_review")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Review extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "review_id")
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false, unique = true)
+    private Order order;
+
+    @Column(nullable = false)
+    private Integer rating;
+
+    private String content;
+}
