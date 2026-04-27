d2f7fe2600efa0aa818cb5c47bf76b6d24381b72
dddd2356 <dddd2356@naver.com>
2026-04-27 10:46:38 +0900

    fix: 숨겨진 가게에 주문 가능한 버그 수정

--- src/main/java/com/sparta/todayeats/store/domain/repository/StoreRepository.java
+++ src/main/java/com/sparta/todayeats/store/domain/repository/StoreRepository.java
@@ -10,6 +10,6 @@ import java.util.UUID;
 
 public interface StoreRepository extends JpaRepository<StoreEntity, UUID> {
 
-    @Query("SELECT s FROM StoreEntity s WHERE s.storeId = :storeId AND s.deletedAt IS NULL")
+    @Query("SELECT s FROM StoreEntity s WHERE s.storeId = :storeId AND s.deletedAt IS NULL AND (s.isHidden IS NULL OR s.isHidden = false)")
     Optional<StoreEntity> findActiveById(@Param("storeId") UUID storeId);
 }
