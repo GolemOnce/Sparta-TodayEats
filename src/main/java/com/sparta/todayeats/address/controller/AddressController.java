2752315165e8d14ce10ff0a2091ef6d768bc7b44
GolemOnce <shh3350@gmail.com>
2026-04-28 12:46:29 +0900

    feat: 배송지 등록 API 구현

---
+++ src/main/java/com/sparta/todayeats/address/controller/AddressController.java
@@ -0,0 +1,43 @@
+package com.sparta.todayeats.address.controller;
+
+import com.sparta.todayeats.address.dto.reqeust.AddressCreateRequest;
+import com.sparta.todayeats.address.dto.response.AddressCreateResponse;
+import com.sparta.todayeats.address.dto.response.AddressPageResponse;
+import com.sparta.todayeats.address.service.AddressService;
+import com.sparta.todayeats.global.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/addresses")
+public class AddressController {
+
+    private final AddressService addressService;
+
+    // 배송지 등록
+    @PostMapping
+    public ResponseEntity<ApiResponse<AddressCreateResponse>> createAddress(
+            @AuthenticationPrincipal UUID userId,
+            @RequestBody AddressCreateRequest request
+    ){
+        AddressCreateResponse response = addressService.createAddress(userId, request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(response));
+    }
+
+    // 배송지 목록 조회
+
+    // 배송지 상세 조회
+
+    // 배송지 수정
+
+    // 기본 배송지 설정
+
+    // 배송지 삭제
+}
