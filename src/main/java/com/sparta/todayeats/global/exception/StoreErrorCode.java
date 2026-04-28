a5b3cd53cfe4179fb2e7552e579404a678560c50
yuna <micha0215@tukorea.ac.kr>
2026-04-28 12:26:13 +0900

    feat: store 단건 조회 시 권한 수정
    CUSTOMER,비로그인: 공개 가게만 노출 / OWNER: 본인거+공게 / MANAGER,MASTER: 전체

--- src/main/java/com/sparta/todayeats/global/exception/StoreErrorCode.java
+++ src/main/java/com/sparta/todayeats/global/exception/StoreErrorCode.java
@@ -10,7 +10,8 @@ public enum StoreErrorCode implements ErrorCode {
     STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "STORE-001", "가게를 찾을 수 없습니다."),
     STORE_ALREADY_EXISTS(HttpStatus.CONFLICT, "STORE-002", "이미 존재하는 가게 이름입니다"),
     STORE_FORBIDDEN(HttpStatus.FORBIDDEN, "STORE-003", "해당 가게에 대한 접근 권한이 없습니다."),
-    INVALID_STORE_NAME(HttpStatus.BAD_REQUEST, "STORE-004", "유효하지 않은 가게 이름입니다.");
+    INVALID_STORE_NAME(HttpStatus.BAD_REQUEST, "STORE-004", "유효하지 않은 가게 이름입니다."),
+    STORE_NOT_VISIBLE(HttpStatus.FORBIDDEN, "STORE-005", "숨김 처리된 가게는 조회할 수 없습니다.");
 
     private final HttpStatus status;
     private final String code;
