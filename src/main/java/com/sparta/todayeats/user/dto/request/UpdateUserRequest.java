dd0bd3b09fe715def727337377458da9dc572ca9
gray-ji <seeu314@gmail.com>
2026-04-28 13:11:41 +0900

    fix: UpdateUserRequest 필드 검증 오류 수정

--- src/main/java/com/sparta/todayeats/user/dto/request/UpdateUserRequest.java
+++ src/main/java/com/sparta/todayeats/user/dto/request/UpdateUserRequest.java
@@ -1,6 +1,7 @@
 package com.sparta.todayeats.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
@@ -13,6 +14,6 @@ public class UpdateUserRequest {
     @Size(min = 2, max = 20, message = "닉네임은 2~20자 사이여야 합니다.")
     private String nickname;
 
-    @NotBlank(message = "정보 공개 여부는 필수입니다.")
+    @NotNull(message = "정보 공개 여부는 필수입니다.")
     private boolean visible;
 }
