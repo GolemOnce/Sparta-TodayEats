bf007a9731ff5ce67c700c6cb936341715d8455b
GolemOnce <shh3350@gmail.com>
2026-04-28 15:01:16 +0900

    refactor: 불필요한 에러코드 삭제

--- src/main/java/com/sparta/todayeats/global/exception/AddressErrorCode.java
+++ src/main/java/com/sparta/todayeats/global/exception/AddressErrorCode.java
@@ -7,8 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum AddressErrorCode implements ErrorCode {
-    ADDRESS_NOT_FOUND(HttpStatus.NOT_FOUND, "ADDR-001", "배송지를 찾을 수 없습니다."),
-    ADDRESS_ACCESS_DENIED(HttpStatus.FORBIDDEN, "ADDR-002", "본인의 배송지만 조회할 수 있습니다.");
+    ADDRESS_NOT_FOUND(HttpStatus.NOT_FOUND, "ADDR-001", "배송지를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String code;
