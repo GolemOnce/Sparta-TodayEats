99edc7e2cfcf50e16f12e643ee05da3646558e5d
gray-ji <seeu314@gmail.com>
2026-04-30 13:57:14 +0900

    docs: ApiResponse/PageResponse Swagger 설정 추가

--- src/main/java/com/sparta/todayeats/global/response/PageResponse.java
+++ src/main/java/com/sparta/todayeats/global/response/PageResponse.java
@@ -1,21 +1,33 @@
 package com.sparta.todayeats.global.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.domain.Page;
 
 import java.util.List;
 
-// 목록 API의 page 응답 DTO
+@Schema(description = "공통 Page 응답 객체")
 @Getter
 @Builder
 public class PageResponse<T> {
-    private List<T> content;    // 데이터 목록
-    private int page;           // 현재 페이지 번호
-    private int size;           // 페이지 당 데이터 개수
-    private long totalElements; // 전체 데이터 개수
-    private int totalPages;     // 전체 페이지 수
-    private String sort;        // 정렬 기준
+    @Schema(description = "데이터 목록")
+    private List<T> content;
+
+    @Schema(description = "현재 페이지 번호", example = "0")
+    private int page;
+
+    @Schema(description = "페이지 당 데이터 개수", example = "20")
+    private int size;
+
+    @Schema(description = "전체 데이터 개수", example = "10")
+    private long totalElements;
+
+    @Schema(description = "전체 페이지 수", example = "1")
+    private int totalPages;
+
+    @Schema(description = "정렬 기준", example = "createdAt,desc")
+    private String sort;
 
     // Page를 PageResponse로 변환
     public static <T, E> PageResponse<T> from(Page<E> page, List<T> content) {
