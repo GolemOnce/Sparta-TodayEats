8aa410678157fb28daeb07280f554400a68f766f
gray-ji <seeu314@gmail.com>
2026-04-30 16:36:38 +0900

    docs: 리뷰 API Swagger 설정 추가

--- src/main/java/com/sparta/todayeats/review/dto/response/ReviewDetailResponse.java
+++ src/main/java/com/sparta/todayeats/review/dto/response/ReviewDetailResponse.java
@@ -1,6 +1,7 @@
 package com.sparta.todayeats.review.dto.response;
 
 import com.sparta.todayeats.review.entity.Review;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.domain.Page;
@@ -8,18 +9,38 @@ import org.springframework.data.domain.Page;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Schema(description = "리뷰 상세 정보 응답")
 @Getter
 @Builder
 public class ReviewDetailResponse {
+    @Schema(description = "리뷰 ID", example = "r10e8400-e29b-41d4-a716-446655447777")
     private UUID reviewId;
+
+    @Schema(description = "주문 ID", example = "770e8400-e29b-41d4-a716-446655440000")
     private UUID orderId;
+
+    @Schema(description = "가게 ID", example = "123e4567-e89b-12d3-a456-426614174000")
     private UUID storeId;
+
+    @Schema(description = "고객 ID", example = "550e8400-e29b-41d4-a716-446655441111")
     private UUID customerId;
-    private int rating;
+
+    @Schema(description = "별점", example = "5")
+    private Integer rating;
+
+    @Schema(description = "내용", example = "만두 피가 얇고 속이 꽉 차서 정말 맛있어요!")
     private String content;
+
+    @Schema(description = "생성 시간", example = "2026-04-30T18:00:00")
     private LocalDateTime createdAt;
+
+    @Schema(description = "생성자 ID", example = "550e8400-e29b-41d4-a716-446655441111")
     private UUID createdBy;
+
+    @Schema(description = "수정 시간", example = "2026-04-30T19:00:00")
     private LocalDateTime updatedAt;
+
+    @Schema(description = "수정자 ID", example = "550e8400-e29b-41d4-a716-446655441111")
     private UUID updatedBy;
 
     public static ReviewDetailResponse from(Review review) {
