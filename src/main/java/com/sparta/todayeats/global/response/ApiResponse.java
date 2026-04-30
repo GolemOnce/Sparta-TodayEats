7de5bd650f36ea09c0c34c0fef1ca1c84a499085
gray-ji <seeu314@gmail.com>
2026-04-30 13:57:14 +0900

    docs: ApiResponse/PageResponse Swagger 설정 추가

--- src/main/java/com/sparta/todayeats/global/response/ApiResponse.java
+++ src/main/java/com/sparta/todayeats/global/response/ApiResponse.java
@@ -1,14 +1,20 @@
 package com.sparta.todayeats.global.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@Schema(description = "공통 응답 객체")
 @Getter
 @AllArgsConstructor
 public class ApiResponse<T> {
-
+    @Schema(description = "HTTP 상태 코드", example = "200")
     private int status;
+
+    @Schema(description = "응답 메시지", example = "SUCCESS")
     private String message;
+
+    @Schema(description = "응답 데이터")
     private T data;
 
     public static <T> ApiResponse<T> created(T data) {
